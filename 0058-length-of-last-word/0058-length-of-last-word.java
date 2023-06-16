8a17940f84112b82ea3313ba676b791bbef78f6c
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-06-16 22:15:21 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (79.76%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+ 
+        int count = 0;
+        s = s.trim();
+        int start = s.length() - 1;
+        for(int i=start; i >= 0; i--){
+            if(s.charAt(i) == ' '){
+                break;
+            } 
+            count++;
+        }
+        return count;       
+    }
+}
