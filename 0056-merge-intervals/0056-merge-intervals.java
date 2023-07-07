19a3833c4faa84f80425275d6723e4c0e228cd42
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-07-08 00:43:35 +0530

    Time: 10 ms (44.97%), Space: 45.7 MB (61.29%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-         Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+      Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
         int index = 0;
         for (int[] interval : intervals) {
-            if (index == 0 || interval[0] > intervals[index - 1][1]) {
-                intervals[index++] = interval;
-            } else {
+            if (index > 0 && interval[0] <= intervals[index - 1][1]) {
                 intervals[index - 1][1] = Math.max(intervals[index - 1][1], interval[1]);
+            } else {
+                intervals[index++] = interval;
             }
         }
 
