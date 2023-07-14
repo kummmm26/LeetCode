19c24aa6bd6715478af45ff5d6d1b5262ec587e3
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-07-14 13:18:10 +0530

    Time: 12 ms (83.31%), Space: 54.8 MB (87.91%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,14 @@
+import java.util.HashSet;
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        boolean dup=false;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length - 1;i++)
-        {
-            if(nums[i]==nums[i+1])
-            {
-                dup=true;
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
             }
+            set.add(num);
         }
-        return dup;
+        return false;
     }
 }
