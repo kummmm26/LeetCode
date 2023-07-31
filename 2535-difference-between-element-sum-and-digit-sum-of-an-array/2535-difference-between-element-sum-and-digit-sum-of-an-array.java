426ca50f620721c2b251f482eb1c2e77f87d0a61
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-08-01 00:22:48 +0530

    Time: 3 ms (75.06%), Space: 44.7 MB (8.22%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.java
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.java
@@ -1,54 +1,27 @@
-// class Solution {
-//     public int differenceOfSum(int[] nums) 
-//     {//2441 2455
-//         int n=nums.length;
-//         int sum=0;
-//         int digitsum=0;
-//         int r=0;
-//         for(int i=0;i<n;i++)
-//         {
-//             sum=sum+nums[i];
-//             if(nums[i]<10)
-//             {
-//                 digitsum=digitsum+nums[i];
-//             }
-//             else
-//             {
-//                 while(nums[i]>0)
-//                 {
-//                    r=nums[i]%10;
-//                    nums[i]=nums[i]/10;
-//                    digitsum=digitsum+r;
-//                 }
-//             }
-//         }
-//         return Math.abs(sum-digitsum);
-//     }
-// }
 class Solution {
-    public int differenceOfSum(int[] nums) {
-        int n = nums.length;
-        int sum = 0;
-        int[] digitSum = new int[10]; // Array to store precalculated digit sum for numbers less than 10
-         int digitsum=0;
-        for (int i = 1; i < 10; i++) {
-            digitSum[i] = i;
-        }
-
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
-            if (nums[i] >= 10) {
-                int num = nums[i];
-                while (num > 0) {
-                    int r = num % 10;
-                    num /= 10;
-                    digitsum += r;
+    public int differenceOfSum(int[] nums) 
+    {//2441 2455
+        int n=nums.length;
+        int sum=0;
+        int digitsum=0;
+        int r=0;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+nums[i];
+            if(nums[i]<10)
+            {
+                digitsum=digitsum+nums[i];
+            }
+            else
+            {
+                while(nums[i]>0)
+                {
+                   r=nums[i]%10;
+                   nums[i]=nums[i]/10;
+                   digitsum=digitsum+r;
                 }
-            } else {
-                digitsum += digitSum[nums[i]]; // Use precalculated digit sum
             }
         }
-
-        return Math.abs(sum - digitsum);
+        return Math.abs(sum-digitsum);
     }
 }
