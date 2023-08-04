a6cc782afb1598b4de83d7669c11be1d1e854de8
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-08-04 22:34:05 +0530

    Time: 1 ms (37.59%), Space: 41.2 MB (87.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,27 +1,10 @@
-class Solution 
-{
-    public void merge(int[] nums1, int m, int[] nums2, int n) 
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n)
     {
-       int insertIndex = m+n-1;
-        int indexA = m-1,indexB = n-1;
-        
-        while(indexB>=0)
-        {
-            if(indexA<0)
-            {
-                nums1[insertIndex--] = nums2[indexB--];
-            }
-            else
-            {
-                if(nums2[indexB]>=nums1[indexA])
-                {
-                    nums1[insertIndex--] = nums2[indexB--];
-                }
-                else
-                {
-                    nums1[insertIndex--] = nums1[indexA--];
-                }
-            }
+         for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
         }
-}
+        Arrays.sort(nums1);
+    }
 }
