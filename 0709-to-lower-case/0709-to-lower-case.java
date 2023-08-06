f6ecdfafe0eee5039d619d958b30fb71ac40a237
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-08-06 15:45:13 +0530

    Time: 1 ms (17.70%), Space: 41 MB (16.40%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,6 +1,16 @@
+// class Solution {
+//     public String toLowerCase(String s)
+//     {
+//         return s.toLowerCase();
+//     }
+// }
 class Solution {
-    public String toLowerCase(String s)
-    {
-        return s.toLowerCase();
+    public String toLowerCase(String s) {
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            ans.append(c > 64 && c < 91 ? (char)(c + 32) : c);
+        }
+        return new String(ans);
     }
 }
