084eac1654024075169d615216608bc3400ed473
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-08-09 00:04:56 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (62.76%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.java
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.java
@@ -1,15 +1,23 @@
 class Solution {
-  public boolean canBeIncreasing(int[] nums) {
+  public boolean canBeIncreasing(int[] nums)
+  {
     boolean removed = false;
 
     for (int i = 1; i < nums.length; ++i)
-      if (nums[i - 1] >= nums[i]) {
+    {
+      if (nums[i - 1] >= nums[i]) 
+      {
         if (removed)
+        {
           return false;
-        removed = true; // Remove nums[i - 1]
+        }
+        removed = true; 
         if (i > 1 && nums[i - 2] >= nums[i])
-          nums[i] = nums[i - 1]; // Remove nums[i] instead
+        {    
+          nums[i] = nums[i - 1]; 
+        }
       }
+    }
 
     return true;
   }
