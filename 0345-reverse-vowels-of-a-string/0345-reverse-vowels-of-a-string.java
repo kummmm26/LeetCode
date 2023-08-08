e3b7509729bdb1bcb68217388014d95bd2420c85
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-08-09 00:29:44 +0530

    Time: 2 ms (99.98%), Space: 43.9 MB (72.75%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,26 +1,53 @@
-class Solution {
-    public String reverseVowels(String s) 
-    {
-        Stack<Character> v=new Stack<Character>();
-        String newstring="";
-        for(int i=0;i<s.length();i++)
-        {
-           if(s.charAt(i)== 'a' || s.charAt(i)=='e' ||s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)== 'A' || s.charAt(i)=='E' ||s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U')
-           {
-               v.push(s.charAt(i));
-           }
-        }
-        for(int i=0;i<s.length();i++)
-        {
-             if(s.charAt(i)== 'a' || s.charAt(i)=='e' ||s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)== 'A' || s.charAt(i)=='E' ||s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U')
-           {
-               newstring +=v.pop();
-           }
-            else
-            {
-                newstring+=s.charAt(i);        }
-        }
-        return newstring;
+// class Solution {
+//     public String reverseVowels(String s) 
+//     {
+//         Stack<Character> v=new Stack<Character>();
+//         String newstring="";
+//         for(int i=0;i<s.length();i++)
+//         {
+//            if(s.charAt(i)== 'a' || s.charAt(i)=='e' ||s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)== 'A' || s.charAt(i)=='E' ||s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U')
+//            {
+//                v.push(s.charAt(i));
+//            }
+//         }
+//         for(int i=0;i<s.length();i++)
+//         {
+//              if(s.charAt(i)== 'a' || s.charAt(i)=='e' ||s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)== 'A' || s.charAt(i)=='E' ||s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U')
+//            {
+//                newstring +=v.pop();
+//            }
+//             else
+//             {
+//                 newstring+=s.charAt(i);        }
+//         }
+//         return newstring;
         
+//     }
+// }
+
+class Solution {
+    public String reverseVowels(String s) {
+        boolean[] vowels = new boolean[128];
+        for (char c : "aeiouAEIOU".toCharArray()) {
+            vowels[c] = true;
+        }
+        char[] cs = s.toCharArray();
+        int i = 0, j = cs.length - 1;
+        while (i < j) {
+            while (i < j && !vowels[cs[i]]) {
+                ++i;
+            }
+            while (i < j && !vowels[cs[j]]) {
+                --j;
+            }
+            if (i < j) {
+                char t = cs[i];
+                cs[i] = cs[j];
+                cs[j] = t;
+                ++i;
+                --j;
+            }
+        }
+        return String.valueOf(cs);
     }
 }
