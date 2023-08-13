ca9f0bd8925b10347e4c199933ceaf00f3601518
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-08-14 02:09:42 +0530

    Time: 1 ms (100.00%), Space: 43.1 MB (84.53%) - LeetHub

--- 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.java
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.java
@@ -1,32 +1,32 @@
-// class Solution {
-//     public int sumOfSquares(int[] nums)
-//     {
-//         int sum=0;
-//         int n=nums.length;
-//         for(int i=0,j=1;i<nums.length;i++,j++)
-//         {
-//             if(n % j==0)
-//             {
-//                 sum = sum + nums[i] * nums[i];
-//             }
-//         }
-//         return sum;
-//     }
-// }
-
 class Solution {
-  public int sumOfSquares(int[] nums) 
-  {
-    int n = nums.length;
-    int ans = 0;
-    for (int i = 0; i < n; ++i)
+    public int sumOfSquares(int[] nums)
     {
-      if (n % (i + 1) == 0)
-      {
-        ans =ans + nums[i] * nums[i];
-      }
+        int sum=0;
+        int n=nums.length;
+        for(int i=0,j=1;i<nums.length;i++,j++)
+        {
+            if(n % j==0)
+            {
+                sum = sum + nums[i] * nums[i];
+            }
+        }
+        return sum;
     }
-
-    return ans;
-  }
 }
+
+// class Solution {
+//   public int sumOfSquares(int[] nums) 
+//   {
+//     int n = nums.length;
+//     int ans = 0;
+//     for (int i = 0; i < n; ++i)
+//     {
+//       if (n % (i + 1) == 0)
+//       {
+//         ans =ans + nums[i] * nums[i];
+//       }
+//     }
+
+//     return ans;
+//   }
+// }
