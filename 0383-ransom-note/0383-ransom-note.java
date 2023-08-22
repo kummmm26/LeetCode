428b52aa3d688b65881329aaf537cdcc6cf8f09d
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-08-22 13:34:14 +0530

    Time: 2 ms (96.66%), Space: 43.8 MB (67.42%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,32 +1,54 @@
-class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        StringBuilder r=new StringBuilder();
-        r.append(ransomNote);
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         StringBuilder r=new StringBuilder();
+//         r.append(ransomNote);
 
-        if(ransomNote.length() > magazine.length())
+//         if(ransomNote.length() > magazine.length())
+//         {
+//              return false;
+//         }
+//             for(int i=0;i<magazine.length();i++)
+//             {
+//                 for(int j=0;j<r.length();j++)
+//                 {
+//                     if(r.charAt(j)== magazine.charAt(i))
+//                        {
+//                            r.setCharAt(j,' ');
+//                            break;
+//                        }
+//                 }
+//             }
+//         for(int i=0;i<r.length();i++)
+//         {
+//             if(!Character.isWhitespace(r.charAt(i)))
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
+class Solution 
+{
+    public boolean canConstruct(String ransomNote, String magazine) 
+    {
+        if (ransomNote.length() > magazine.length()) 
         {
-             return false;
+            return false;
         }
-        else
-        {
-            for(int i=0;i<magazine.length();i++)
-            {
-                for(int j=0;j<r.length();j++)
-                {
-                    if(r.charAt(j)== magazine.charAt(i))
-                       {
-                           r.setCharAt(j,' ');
-                           break;
-                       }
-                }
+
+        int[] charCount = new int[26]; 
+
+        for (char c : magazine.toCharArray()) {
+            charCount[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (charCount[c - 'a'] > 0) {
+                charCount[c - 'a']--;
+            } else {
+                return false;
             }
         }
-       // System.out.print(r.toString());
-        for(int i=0;i<r.length();i++)
-        {
-            if(!Character.isWhitespace(r.charAt(i)))
-                return false;
-        }
+
         return true;
     }
 }
