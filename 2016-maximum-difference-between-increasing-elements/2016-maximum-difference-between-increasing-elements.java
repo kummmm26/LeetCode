504f6b96f8ad49765d677eba63e64c5664acd0f2
kumkum verma <79091238+kummmm26@users.noreply.github.com>
2023-08-25 03:08:37 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (60.50%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
@@ -1,21 +1,33 @@
+// class Solution {
+//     public int maximumDifference(int[] nums)
+//     {
+//         int ans=-1,diff;
+//         for(int i=nums.length-1;i>0;i--)
+//         {
+//             for(int j=i-1;j>=0;j--)
+//             {
+//                 if(nums[i]>nums[j])
+//                 {
+//                     diff=nums[i]-nums[j];
+//                     if(diff>ans)
+//                     {
+//                         ans=diff;
+//                     }
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
-    public int maximumDifference(int[] nums)
-    {
-        int ans=-1,diff;
-        for(int i=nums.length-1;i>0;i--)
-        {
-            for(int j=i-1;j>=0;j--)
-            {
-                if(nums[i]>nums[j])
-                {
-                    diff=nums[i]-nums[j];
-                    if(diff>ans)
-                    {
-                        ans=diff;
-                    }
-                }
-            }
+    public int maximumDifference(int[] nums) {
+        int res=-1;
+        int minEle = Integer.MAX_VALUE;
+        for(int i=0; i<nums.length; i++) {
+            minEle = Math.min(minEle,nums[i]);
+            if(nums[i]!=minEle)
+                res=Math.max(res,nums[i]-minEle);
         }
-        return ans;
+        return Math.max(res,-1);
     }
 }
